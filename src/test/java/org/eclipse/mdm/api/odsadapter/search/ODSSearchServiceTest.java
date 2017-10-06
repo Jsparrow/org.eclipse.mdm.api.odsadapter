8256dc7462044dd6e4ec6d6f450a007d337432a2
Florian Schmitt <florian.schmitt@atos.net>
2017-10-06 15:47:47 +0200

    525681: example for access to relations
    Change-Id: I6d1c164882f8cae3d8701a4a771e4fca5c169717
    Signed-off-by: Florian Schmitt <florian.schmitt@atos.net>

--- src/test/java/org/eclipse/mdm/api/odsadapter/search/ODSSearchServiceTest.java
+++ src/test/java/org/eclipse/mdm/api/odsadapter/search/ODSSearchServiceTest.java
@@ -6,29 +6,51 @@ import static org.eclipse.mdm.api.odsadapter.ODSEntityManagerFactory.PARAM_NAMES
 import static org.eclipse.mdm.api.odsadapter.ODSEntityManagerFactory.PARAM_PASSWORD;
 import static org.eclipse.mdm.api.odsadapter.ODSEntityManagerFactory.PARAM_SERVICENAME;
 import static org.eclipse.mdm.api.odsadapter.ODSEntityManagerFactory.PARAM_USER;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.assertj.core.api.iterable.Extractor;
 import org.assertj.core.groups.Tuple;
 import org.eclipse.mdm.api.base.ConnectionException;
+import org.eclipse.mdm.api.base.Transaction;
+import org.eclipse.mdm.api.base.model.BaseEntity;
+import org.eclipse.mdm.api.base.model.Channel;
+import org.eclipse.mdm.api.base.model.ChannelGroup;
+import org.eclipse.mdm.api.base.model.Core;
+import org.eclipse.mdm.api.base.model.Measurement;
+import org.eclipse.mdm.api.base.model.ParameterSet;
+import org.eclipse.mdm.api.base.model.Test;
 import org.eclipse.mdm.api.base.model.TestStep;
+import org.eclipse.mdm.api.base.model.Core.EntityStore;
 import org.eclipse.mdm.api.base.query.EntityType;
 import org.eclipse.mdm.api.base.query.Filter;
 import org.eclipse.mdm.api.base.query.FilterItem;
 import org.eclipse.mdm.api.base.query.ModelManager;
+import org.eclipse.mdm.api.base.query.Record;
+import org.eclipse.mdm.api.base.query.Result;
+import org.eclipse.mdm.api.base.query.SearchService;
 import org.eclipse.mdm.api.base.query.BracketOperator;
 import org.eclipse.mdm.api.base.query.ComparisonOperator;
+import org.eclipse.mdm.api.base.query.DataAccessException;
 import org.eclipse.mdm.api.base.query.BooleanOperator;
 import org.eclipse.mdm.api.dflt.EntityManager;
+import org.eclipse.mdm.api.dflt.model.Pool;
+import org.eclipse.mdm.api.dflt.model.Project;
 import org.eclipse.mdm.api.odsadapter.ODSEntityManagerFactory;
+import org.eclipse.mdm.api.odsadapter.query.ODSModelManager;
+import org.eclipse.mdm.api.odsadapter.search.JoinTree.JoinConfig;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
-import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.google.common.collect.ImmutableMap;
@@ -90,7 +112,106 @@ public class ODSSearchServiceTest {
 			entityManager.close();
 		}
 	}
+	
+	final class MeasurementSearchQuery extends BaseEntitySearchQuery {
 
+		/**
+		 * Constructor.
+		 *
+		 * @param modelManager
+		 *            Used to load {@link EntityType}s.
+		 * @param contextState
+		 *            The {@link ContextState}.
+		 */
+		MeasurementSearchQuery(ODSModelManager modelManager, ContextState contextState) {
+			super(modelManager, ParameterSet.class, Project.class);
+
+			// layers
+			addJoinConfig(JoinConfig.up(Pool.class, Project.class));
+			addJoinConfig(JoinConfig.up(Test.class, Pool.class));
+			addJoinConfig(JoinConfig.up(TestStep.class, Test.class));
+			addJoinConfig(JoinConfig.up(Measurement.class, TestStep.class));
+			addJoinConfig(JoinConfig.up(ParameterSet.class, Measurement.class));
+			addJoinConfig(JoinConfig.down(Measurement.class, Channel.class));
+			addJoinConfig(JoinConfig.down(Measurement.class, ChannelGroup.class));
+
+			// context
+			addJoinConfig(contextState);
+		}
+
+	}
+	
+	/**
+	 * changes a relation between instances. There needs to exist a MeaResult of id 1101 and a ParameterSet of id
+	 * 1102 which will be related after running this test.
+	 * If these don't exist, please leave the following line commented, or the test will fail.  
+	 * @throws Exception 
+	 */
+	//@org.junit.Test
+	public void changeRelation() throws Exception {
+		String idmea = "1101";
+		String idparamset = "1002";
+		ModelManager modelManager = entityManager.getModelManager().get();
+		SearchService searchService = entityManager.getSearchService().get();
+
+		EntityType etmeasurement = modelManager.getEntityType(Measurement.class);
+		EntityType etparamset = modelManager.getEntityType(ParameterSet.class);
+		Transaction transaction;
+
+		transaction = entityManager.startTransaction();
+
+		try {
+			List<Measurement> mealist;
+			mealist = searchService.fetch(Measurement.class, Filter.idOnly(etmeasurement, idmea));
+			assertEquals(1, mealist.size());
+			Measurement mea = mealist.get(0);
+
+			List<ParameterSet> paramsetlist = new ArrayList<>();
+			ParameterSet paramset = entityManager.load(ParameterSet.class, idparamset);
+			paramsetlist.add(paramset);
+
+			// FIXME: the API should expose the getCore method so that we don't
+			// need to use introspection
+			// to access the core.
+			Method GET_CORE_METHOD;
+			try {
+				GET_CORE_METHOD = BaseEntity.class.getDeclaredMethod("getCore");
+				GET_CORE_METHOD.setAccessible(true);
+			} catch (NoSuchMethodException | SecurityException e) {
+				throw new IllegalStateException(
+						"Unable to load 'getCore()' in class '" + BaseEntity.class.getSimpleName() + "'.", e);
+			}
+			Core core = (Core) GET_CORE_METHOD.invoke(paramset);
+			// Note: we can only set relations in the mutable store (which
+			// doesn't include parent-child-relations)
+			EntityStore store = core.getMutableStore();
+			store.set(mea);
+			assertEquals(core.getMutableStore().get(Measurement.class), mea);
+
+			transaction.update(paramsetlist);
+			transaction.commit();
+
+			// reload from database and check if relation is consistent with
+			// expectations
+			// first we need to build our own SearchQuery, because the
+			// predefined ones don't include ParameterSet
+			MeasurementSearchQuery mq = new MeasurementSearchQuery((ODSModelManager) modelManager,
+					ContextState.MEASURED);
+			List<EntityType> fetchList = new ArrayList<>();
+			fetchList.add(etmeasurement);
+			List<Result> fetch = mq.fetchComplete(fetchList, Filter.idOnly(etparamset, idparamset));
+			assertEquals(fetch.size(), 1);
+			Record record = fetch.get(0).getRecord(etmeasurement);
+			assertNotNull(record);
+			assertEquals(record.getID(), idmea);
+
+		} catch (RuntimeException | IllegalAccessException | InvocationTargetException | DataAccessException e) {
+			transaction.abort();
+			throw e;
+		}
+
+	}
+	
 	private Extractor<FilterItem, Tuple> filterExtractors = new Extractor<FilterItem, Tuple>() {
 
 		@Override
@@ -103,7 +224,7 @@ public class ODSSearchServiceTest {
 		}
 	};
 
-	@Test
+	@org.junit.Test
 	public void testGetMergedFilter() throws Exception {
 
 		ODSSearchService service = Mockito.spy((ODSSearchService) entityManager.getSearchService()
@@ -124,7 +245,7 @@ public class ODSSearchServiceTest {
 						tuple(null, null, null, null, BracketOperator.CLOSE));
 	}
 
-	@Test
+	@org.junit.Test
 	public void testGetMergedFilterNoAttributeFilter() throws Exception {
 		ODSSearchService service = Mockito.spy((ODSSearchService) entityManager.getSearchService().get());
 
@@ -135,7 +256,7 @@ public class ODSSearchServiceTest {
 				.containsExactly(tuple(null, "Id", ComparisonOperator.IN_SET, new String[] { "10" }));
 	}
 
-	@Test
+	@org.junit.Test
 	public void testGetMergedFilterNoFreetextResult() throws Exception {
 		ODSSearchService service = Mockito.spy((ODSSearchService) entityManager.getSearchService().get());
 
@@ -147,7 +268,7 @@ public class ODSSearchServiceTest {
 				.containsExactly(tuple(null, "Id", ComparisonOperator.EQUAL, "11"));
 	}
 
-	@Test
+	@org.junit.Test
 	public void testGetMergedFilterNoAttributeFilterAndNoFreetextResult() throws Exception {
 		ODSSearchService service = Mockito.spy((ODSSearchService) entityManager.getSearchService().get());
 
